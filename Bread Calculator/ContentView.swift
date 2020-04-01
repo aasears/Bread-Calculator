976b1901f459bd772d471f181e347fcffa0dcffa
Aaron Sears <aeroaaron200@gmail.com>
2020-04-01 17:29:41 -0500

    Initial Commit

---
+++ Bread Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bread Calculator
+//
+//  Created by Aaron Sears on 4/1/20.
+//  Copyright © 2020 Aaron Sears. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
